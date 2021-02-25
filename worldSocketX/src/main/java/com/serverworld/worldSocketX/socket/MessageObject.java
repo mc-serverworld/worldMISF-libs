450842e0023ff64a015efe73c169a974ffec423e
admin <paul002706@gmail.com>
2021-02-25 11:15:34 +0800

    upgrade to TLS v1.3 upgrade cert keysize to R4096 upgrade to sha512RSA
    Enable Protocols:
    TLS_AES_256_GCM_SHA384
    TLS_CHACHA20_POLY1305_SHA256
    TLS_AES_128_GCM_SHA256

---
+++ worldSocketX/src/main/java/com/serverworld/worldSocketX/socket/MessageObject.java
@@ -0,0 +1,12 @@
+package com.serverworld.worldSocketX.socket;
+
+import java.util.UUID;
+
+public class MessageObject {
+    public MessageObject(String message, UUID receiver){
+
+    }
+    public MessageObject(String message, String channel){
+
+    }
+}
